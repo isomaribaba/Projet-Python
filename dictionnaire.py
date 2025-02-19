9f841ac3401fc5aae10dc3460ac91bee3ea6cf23
hackerone <isomaribaba@outlook.fr>
2025-02-19 23:42:26 +0100

    Test 1

---
+++ dictionnaire.py
@@ -0,0 +1,20 @@
+print ("On va cree un dictionnaire")
+etudiants ={
+    1 : {
+        'nom' : 'Ismaila',
+        'prenom' : 'Ba',
+        'Adresse' : 'yeumbeul',
+    },
+    2 : {
+        'nom' : 'Omar Sylla',
+        'prenom' : 'Ba',
+        'Adresse' : 'yeumbeul',
+    },
+    3 : {
+        'nom' : 'Mouhamed Zakaria',
+        'prenom' : 'Ba',
+        'Adresse' : 'yeumbeul',
+    },
+}
+
+print (etudiants)
