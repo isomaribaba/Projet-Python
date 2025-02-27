19cce75473aef985160c58a4fafe533e9e1c0874
hackerone <isomaribaba@outlook.fr>
2025-02-27 22:11:01 +0100

    Test 1

--- jeux.py
+++ jeux.py
@@ -1,6 +1,6 @@
 #creation d'un jeux au hasard
 #on importe de random pour choisir un nombre aleatoirement choisir entre 1 à 100
-import random
+#***import random
 nombre_aleatoire = random.randint(1, 100)
 nombre_donne = 0
 nombre_daisse = 5
