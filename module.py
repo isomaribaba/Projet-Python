5f996ae183311a68ded9ecfacce0f06cafb0caeb
hackerone <isomaribaba@outlook.fr>
2025-03-08 21:32:48 +0100

    teste5

--- module.py
+++ module.py
@@ -1,4 +1,10 @@
 import os
+import requests
+import scapy
+
+
 
 print(f"Vous etez dans le répertoire {os.getcwd()}")
 print(os.listdir())
+requet = requests.get('https://gsimel.com/')
+print(requet.content)
